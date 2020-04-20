7e497809c1bc48ad851138ba7d5379e4781c9698
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-20 15:50:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    var newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number){
+    let countDown = 0;
+    while (countDown <= number) {
+        console.log(number--)
+    }
+}
